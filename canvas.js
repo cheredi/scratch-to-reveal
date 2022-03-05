3282e09263666ac1c99935abf8fa17fde3ff57e3
cheredi <tatyanaamugo07@gmail.com>
2022-03-05 16:07:56 +0300

    m

--- canvas.js
+++ canvas.js
@@ -12,7 +12,7 @@
       brush        = new Image();
       
     // base64 Workaround because Same-Origin-Policy
-    image.src = 'img/foregroundUpdated.png'
+    image.src = 'img/Foreground2.png'
     image.onload = function() {
     ctx.drawImage(image, 0, 0);
     // Show the form when Image is loaded.
